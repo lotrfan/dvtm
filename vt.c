667f179b19bc4763f8fea5a46f059952b2434ce7
Jeffrey Tolar <tolar.jeffrey@gmail.com>
2014-11-19 23:53:21 -0600

    Update backspace/delete

--- vt.c
+++ vt.c
@@ -212,9 +212,9 @@ static const char *keytable[KEY_MAX+1] = {
 #endif
 	[KEY_SRIGHT]    = "\e[1;2C",
 	[KEY_SLEFT]     = "\e[1;2D",
-	[KEY_BACKSPACE] = "\177",
+	[KEY_BACKSPACE] = "\x08",
 	[KEY_IC]        = "\e[2~",
-	[KEY_DC]        = "\e[3~",
+	[KEY_DC]        = "\x7f",
 	[KEY_PPAGE]     = "\e[5~",
 	[KEY_NPAGE]     = "\e[6~",
 	[KEY_HOME]      = "\e[7~",
